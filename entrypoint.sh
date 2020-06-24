99ed1fb8c9056f1f54fd08b1022bd5cbf8d3e43d
Biplob SD <43641536+biplobsd@users.noreply.github.com>
2020-06-25 00:43:06 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,9 +19,6 @@ WORKDIR=${SRCDIR:-/src}
 
 SPEC_FILE="${SRCDIR}/${4}"
 
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-choco install visualcpp-build-tools
-
 python -m pip install --upgrade pip wheel setuptools
 
 #
