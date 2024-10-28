73349115be7b21427dbe7cff09b55b57df1ec783
xKRISTOFx mobile <gral1322@gmail.com>
2024-10-28 19:36:55 +0100

    fix gcc

--- cython_build.py
+++ cython_build.py
@@ -14,7 +14,7 @@ from Cython.Build import cythonize
 # Append "build_ext" and "--inplace" to sys.argv to ensure the extension is built in place
 argv.append("build_ext")
 argv.append("--inplace")
-argv.append("--compiler=gcc")
+argv.append("--compiler=mingw32")
 print("arguments: ", argv)
 
 try:
